4e17dfb47132429fb30625623e5721b453ff6a53
larisaanilova <129681122+larisaanilova@users.noreply.github.com>
2023-04-10 17:44:27 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -2,15 +2,13 @@
 int numberA = int.Parse(Console.ReadLine());
 Console.Write("Введите второе число: ");
 int numberB = int.Parse(Console.ReadLine());
-int min = numberA;
-int max = numberB;
-if (numberA>numberB)
-{
-    max = numberA;
-    min = numberB;
-}
-Console.Write("Минимальное число: ");
-Console.WriteLine(min);
+Console.Write("Введите третье число: ");
+int numberC = int.Parse(Console.ReadLine());
+int max = numberA;
+if (numberA>max) max = numberA;
+if (numberB>max) max = numberB;
+if (numberC>max) max = numberC;
+   
 Console.Write("Максимальное число: ");
 Console.WriteLine(max);
 
