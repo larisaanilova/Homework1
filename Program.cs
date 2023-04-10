0350e980593aea88410029ae7244aa78b7a07642
larisaanilova <129681122+larisaanilova@users.noreply.github.com>
2023-04-10 16:22:36 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите первое число: ");
+int numberA = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int numberB = int.Parse(Console.ReadLine());
+int min = numberA;
+int max = numberB;
+if (numberA>numberB)
+{
+    max = numberA;
+    min = numberB;
+}
+Console.Write("Минимальное число: ");
+Console.WriteLine(min);
+Console.Write("Максимальное число: ");
+Console.WriteLine(max);
+
